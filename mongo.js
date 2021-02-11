801da02ef7b66fffbb93fae805483b3cfd9e4d49
Carlos Huerta <carlos.h.huertag@gmail.com>
2021-02-11 10:25:18 -0300

    hrk

--- mongo.js
+++ mongo.js
@@ -6,12 +6,12 @@ var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
 db.once("open", () => {});
 
-var usuarioSchema = new mongoose.Schema({
-  nombre: String,
-  edad: Number,
-});
+// var usuarioSchema = new mongoose.Schema({
+//   nombre: String,
+//   edad: Number,
+// });
 
-var Usuario = mongoose.model("Usuario", usuarioSchema);
+// var Usuario = mongoose.model("Usuario", usuarioSchema);
 
-var unUsuario = new Usuario({ nombre: "Carlos", edad: 30 });
-console.log(unUsuario)
+// var unUsuario = new Usuario({ nombre: "Carlos", edad: 30 });
+// console.log(unUsuario)
